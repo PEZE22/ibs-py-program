d67d3720e54097898985b51b98fd95ac22f52202
PEZE22 <125920626+PEZE22@users.noreply.github.com>
2023-03-24 17:41:48 +0100

    Create eze.py

---
+++ eze.py
@@ -0,0 +1,149 @@
+print("hello")
+print(42)
+print(3.141592)
+print(3.1415) 
+print(-1.5)  
+print(0.0)   
+
+
+print(.5) 
+print(5.) 
+
+
+
+print(1 + 1)  
+print(5 - 2)  
+print(3 * 4)  
+print(6 / 2) 
+print(5 / 2)  
+print(5 // 2)
+print(5 % 2)  
+
+
+print(True)  
+print(False)
+
+
+print(not True) 
+print(not False)
+
+
+print(True and False)  
+print(False and True)  
+print(False and False) 
+
+
+print(True or True)   
+print(True or False)  
+print(False or True)  
+print(False or False) 
+
+
+print("apple") 
+print("don't")
+print("\"Everything you can imagine is real.\" - Picasso") 
+
+
+print("tooth" + "brush") 
+
+
+welcome = "Hello, World"
+print(welcome)
+
+
+is_awesome = True
+print(is_awesome)
+
+
+the_meaning_of_life_and_the_universe_and_everything = 42
+print(the_meaning_of_life_and_the_universe_and_everything)
+
+
+pi = 3.141592
+print(pi)
+
+
+number = 12
+
+number = 23
+
+
+
+print("My favorite number is: " + str(8)) 
+
+
+
+
+a = 12
+a += 4
+print(a) 
+
+b = 12
+b -= 4
+print(b)
+
+c = 12
+c *= 3
+print(c) 
+
+d = 12
+d /= 3
+print(d)
+
+e = 12
+e %= 7
+print(e)    5555555555555555555555555
+
+
+
+user_input_1 = input()
+
+print(user_input_1)
+
+user_input_2 = int(input())
+
+print(user_input_2)
+
+a = 13
+
+if a == 13:
+  print("Yaaay! The value of the \"a\" variable is 13") 
+
+if a == 8:
+  print("Yaaay! The value of the \"a\" variable is 8") 
+
+
+b = 20
+
+if b < 10:
+  print("Yaaay! The value of the \"b\" variable is lower than 10")  555555555555555555555
+else:
+  print("Yaaay! The value of the \"b\" variable is higher than 10") 
+
+c = 15
+
+if c < 10:
+  print("Yaaay! The value of the \"b\" variable is lower than 10")
+elif c < 20:
+  print("Yaaay! The value of the \"b\" variable is higher than 10") 
+  print("Yaaay! The value of the \"b\" variable is higher than 10") 
+
+
+thirsty = True
+hungry = False
+
+if thirsty and hungry:
+  print("Lunch time!")
+elif thirsty or hungry:
+  print("Snack time!")
+else:
+  print("No food for you, little fatty.")
+
+
+a = 0:
+while a < 10:
+  print(a)
+  a += 1
+
+for i in range(0, 100):
+  print(i)
